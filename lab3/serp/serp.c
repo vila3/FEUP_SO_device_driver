989838a406817290e1856fe511fd214ff7ec448e
pcova <pedrocova96@gmail.com>
2017-05-19 11:31:13 +0100

    Lab 3 working as expected!

--- lab3/serp/serp.c
+++ lab3/serp/serp.c
@@ -15,16 +15,22 @@
 #include "serial_reg.h"
 MODULE_LICENSE("Dual BSD/GPL");
 
+struct serp_dev {
+	struct cdev cdev;
+	int cnt;
+	int check_timeout;
+};
+
 static int device_open(struct inode *inodep, struct file *filep);
 static int device_release(struct inode *inodep, struct file *filep);
 ssize_t device_read(struct file *filep, char __user *buff, size_t count, loff_t *offp);
 ssize_t device_write(struct file *filep, const char __user *buff, size_t count, loff_t *offp);
 
 #define UART_BASE	0x03F8	// UART's base I/O port-address (COM1)
+#define SERP_COUNT	1
 
 dev_t dev_buffer;
-static struct cdev *mcdev;
-int check_end = 0;
+struct serp_dev *dev;
 const char driver_name[] = "serp";
 
 struct file_operations fops = {
@@ -53,16 +59,24 @@ int sendMessage(char *message, int count)
 
 static int device_open(struct inode *inodep, struct file *filep)
 {
-	filep->private_data = mcdev;
-	printk(KERN_ALERT "Device was open.\n");
+	struct serp_dev *mdev;
+	mdev = container_of(inodep->i_cdev, struct serp_dev, cdev);
+	filep->private_data = mdev;
+	printk(KERN_ALERT " -k Device was open.\n");
 	nonseekable_open(inodep, filep);
+
+	// empty the UART buffer if exist
+	while((inb(UART_BASE + UART_LSR) & (UART_LSR_DR | UART_LSR_OE)) != 0) inb(UART_BASE + UART_RX);
 	return 0;
 }
 
 static int device_release(struct inode *inodep, struct file *filep)
 {
+	struct serp_dev *mdev;
+	mdev = filep->private_data;
+	mdev->check_timeout = 0;
 	filep->private_data = NULL;
-	printk(KERN_ALERT "Device was release 1.\n");
+	printk(KERN_ALERT " -k Device was release.\n");
 	return 0;
 }
 
@@ -72,15 +86,15 @@ ssize_t device_write(struct file *filep, const char __user *buff, size_t count, 
 	int n = copy_from_user(mBuff, buff, count);
 	int i = 0;
 	if(n!=0) {
-		printk(KERN_ALERT "Ups! Couldn't get data to write! (%d)", n);
+		printk(KERN_ALERT " -k Ups! Couldn't get data to write! (%d)", n);
 		return 0;
 	}
 	while (1) {
-		if (message[i] == '\0' || (i >= count))
+		if (mBuff[i] == '\0' || (i >= count))
 			break;
 		while ((inb(UART_BASE + UART_LSR) & UART_LSR_THRE) == 0)
 			msleep_interruptible(1);
-		outb(message[i++], UART_BASE + UART_TX);
+		outb(mBuff[i++], UART_BASE + UART_TX);
 	}
 	kfree(mBuff);
 	return i;
@@ -88,39 +102,45 @@ ssize_t device_write(struct file *filep, const char __user *buff, size_t count, 
 
 ssize_t device_read(struct file *filep, char __user *buff, size_t count, loff_t *offp)
 {
+	struct serp_dev *mdev;
 	char *mBuff = kzalloc(sizeof(char)*count, GFP_KERNEL);
 	unsigned char line_status;
 	int n = 0, n_nothing = 0;
 	if (!mBuff) {
 		return -1;
 	}
+	mdev = filep->private_data;
+
 	while (1) {
 		line_status = inb(UART_BASE + UART_LSR);
-		if ((line_status & UART_LSR_OE) != 0) {
-			kfree(mBuff);
-			printk(KERN_ALERT "Overrun error!");
-			return -EIO;
-		} else if ((line_status & UART_LSR_DR) == 0) {
+		if ((line_status & UART_LSR_DR) == 0) {
 			// printk(KERN_ALERT "Waiting...");
 			msleep_interruptible(1);
 			n_nothing++;
-			// printk(KERN_ALERT "(%d, %d)\n", n_nothing, n);
-			if (n_nothing > 100 && check_end) {
-				// printk(KERN_ALERT "Waiting too long for a char, exiting!\n");
-				check_end = 0;
+			// printk(KERN_ALERT "(%d, %d)\n", n_nothing, mdev->check_timeout);
+			if (n_nothing > 100 && mdev->check_timeout) {
+				if (n==0) {
+					printk(KERN_ALERT " -k Waiting too long for a char, exiting!\n");
+					return 0;
+				}
 				break;
 			}
 		} else {
+			if ((line_status & UART_LSR_OE) != 0) {
+				kfree(mBuff);
+				printk(KERN_ALERT "Overrun error!");
+				return -EIO;
+            }
 			mBuff[n++] = inb(UART_BASE + UART_RX);
-			mBuff[n] = '\0';
+			// printk(KERN_ALERT "%c", mBuff[n-1]);
 			if (n == count)
 				break;
 			n_nothing = 0;
-			check_end = 1;
+			mdev->check_timeout = 1;
 		}
 	}
 	if (copy_to_user(buff, mBuff, n) > 0) {
-		printk(KERN_ALERT "Error copy data to user...\n");
+		printk(KERN_ALERT " -k Error copy data to user...\n");
 		return -1;
 	}
 	kfree(mBuff);
@@ -132,34 +152,38 @@ static int echo_init(void)
 	int vft;
 	struct resource *rsc;
 	unsigned char lcr;
-	vft = alloc_chrdev_region(&dev_buffer, 0, 1, driver_name);
+	vft = alloc_chrdev_region(&dev_buffer, 0, SERP_COUNT, driver_name);
 	if (vft < 0) {
-	  printk(KERN_INFO "Major number allocation is failed\n");
+	  printk(KERN_INFO " -k Major number allocation is failed\n");
 	  return vft;
   	}
-	printk(KERN_ALERT "Device number: <%d, %d>\n", MAJOR(dev_buffer), MINOR(dev_buffer));
+	printk(KERN_ALERT " -k Device number: <%d, %d>\n", MAJOR(dev_buffer), MINOR(dev_buffer));
 
-	mcdev = cdev_alloc();
-	mcdev->ops = &fops;
-	mcdev->owner = THIS_MODULE;
+	dev = kzalloc(sizeof(struct serp_dev)*SERP_COUNT, GFP_KERNEL);
 
-	vft = cdev_add(mcdev, dev_buffer, 1);
+	// in this case only care about one device
+	cdev_init(&dev[0].cdev, &fops);
+	dev[0].cdev.ops = &fops;
+	dev[0].cdev.owner = THIS_MODULE;
+	dev[0].check_timeout = 0;
+
+	vft = cdev_add(&dev[0].cdev, dev_buffer, 1);
 	if (vft<0) {
-		printk(KERN_ALERT "Device adding to the kerknel failed\n");
+		printk(KERN_ALERT " -k Device adding to the kerknel failed\n");
         return vft	;
 	}
-	printk(KERN_INFO "Device additin to the kernel succesful\n");
+	printk(KERN_INFO " -k Device additin to the kernel succesful\n");
 
-	printk(KERN_ALERT "Requestting control over serial port (COM1)...\n");
+	printk(KERN_ALERT " -k Requestting control over serial port (COM1)...\n");
 	rsc = request_region(UART_BASE, 1, driver_name);
 	if (rsc == NULL)
 	{
-		printk(KERN_ALERT "Could not get control over serial port (COM1)!\n");
-		cdev_del(mcdev);
+		printk(KERN_ALERT " -k Could not get control over serial port (COM1)!\n");
+		cdev_del(&dev[0].cdev);
 		unregister_chrdev_region(dev_buffer, 1);
 		return -1;
 	}
-	printk(KERN_ALERT "Gained control over serial port (COM1)!\n");
+	printk(KERN_ALERT " -k Gained control over serial port (COM1)!\n");
 
 	lcr = UART_LCR_DLAB | UART_LCR_WLEN8 | UART_LCR_STOP | UART_LCR_EPAR;
 	outb(lcr, UART_BASE + UART_LCR);
@@ -169,7 +193,7 @@ static int echo_init(void)
 
 	sendMessage("Connection established successfully!\n", 0);
 
-	printk(KERN_ALERT "Connection established successfully!\n");
+	printk(KERN_ALERT " -k Connection established successfully!\n");
 
 	return 0;
 }
@@ -178,7 +202,8 @@ static void echo_exit(void)
 {
 	int major_num = MAJOR(dev_buffer);
 	release_region(UART_BASE, 1);
-	cdev_del(mcdev);
+	cdev_del(&dev[0].cdev);
+	kfree(dev);
 	unregister_chrdev_region(dev_buffer, 1);
 	printk(KERN_ALERT "Unregisted device driver %d (%s)\n", major_num, driver_name);
 }
--- lab3/serp/serp.mod.c
+++ lab3/serp/serp.mod.c
@@ -17,20 +17,22 @@ static const struct modversion_info ____versions[]
 __attribute_used__
 __attribute__((section("__versions"))) = {
 	{ 0x8d0ae4f, "struct_module" },
-	{ 0xf2a644fb, "copy_from_user" },
-	{ 0x37a0cba, "kfree" },
 	{ 0x2da418b5, "copy_to_user" },
-	{ 0xcc5005fe, "msleep_interruptible" },
+	{ 0xf2a644fb, "copy_from_user" },
 	{ 0x12f237eb, "__kzalloc" },
+	{ 0x2323fd40, "nonseekable_open" },
 	{ 0x852abecf, "__request_region" },
 	{ 0xd85a396a, "cdev_add" },
-	{ 0x2be5747e, "cdev_alloc" },
+	{ 0xbb37bf2e, "cdev_init" },
+	{ 0xc818ff24, "kmem_cache_zalloc" },
+	{ 0xb13d73c9, "malloc_sizes" },
 	{ 0x29537c9e, "alloc_chrdev_region" },
+	{ 0xcc5005fe, "msleep_interruptible" },
 	{ 0x7485e15e, "unregister_chrdev_region" },
+	{ 0x37a0cba, "kfree" },
 	{ 0x4bd6e07e, "cdev_del" },
 	{ 0xef79ac56, "__release_region" },
 	{ 0xb407b205, "ioport_resource" },
-	{ 0x2323fd40, "nonseekable_open" },
 	{ 0x1b7d4074, "printk" },
 };
 
